763da5bbbbb660d9ea3f2da50e35b450e23fbe9e
kirim-Lee <uthi1004@gmail.com>
2019-07-17 01:18:22 +0900

    validate tx

--- src/transaction.ts
+++ src/transaction.ts
@@ -16,7 +16,7 @@ class TxOut {
 class TxIn {
   public txOutId: string;
   public txOutIndex: number;
-  public signature: string[];
+  public signature: string;
   // txOutId
   // txOutIndex
   // signature
@@ -59,7 +59,7 @@ const getTxId = (tx: Transaction): string => {
 const findUTxOut = (txOutId: string, txOutIndex: number, uTxOutList: UTxOut[]): UTxOut | undefined =>
   uTxOutList.find(uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex);
 
-const signTxIn = (tx: Transaction, txInIndex: number, privateKey: string): string[] => {
+const signTxIn = (tx: Transaction, txInIndex: number, privateKey: string): string => {
   const txIn: TxIn = tx.txIns[txInIndex];
   const dataToSign = tx.id;
   const referencedUTxOut = findUTxOut(txIn.txOutId, txIn.txOutIndex, uTxOuts);
@@ -67,7 +67,7 @@ const signTxIn = (tx: Transaction, txInIndex: number, privateKey: string): strin
     return;
   }
   const key = ec.keyFromPrivate(privateKey, 'hex');
-  const signature = toHexString(key.sign(dataToSign).toDER());
+  const signature = toHexString(key.sign(dataToSign).toDER()).toString();
   return signature;
 };
 
@@ -95,4 +95,71 @@ const updateUTxOuts = (newTxs: Transaction[], uTxOutList: UTxOut[]) => {
   const resultingUTxOuts = uTxOutList
     .filter(uTxOut => !findUTxOut(uTxOut.txOutId, uTxOut.txOutIndex, spentTxOuts))
     .concat(newUTxOuts);
+};
+
+// TxIn 구조검사
+const isTxInStructurValid = (txIn: TxIn): boolean => {
+  if (txIn === null) {
+    console.log('txIn is null');
+    return false;
+  } else if (typeof txIn.signature === 'string') {
+    console.log('typeof txIn signature invalid');
+    return false;
+  } else if (typeof txIn.txOutId !== 'string') {
+    console.log('typeof txIn txOutId invalid');
+    return false;
+  } else if (typeof txIn.txOutIndex !== 'number') {
+    console.log('typeof txIn txOutIndex invalid');
+    return false;
+  }
+  return true;
+};
+
+// TxOut 구조검사
+const isTxOutStructurValid = (txOut: TxOut) => {
+  if (txOut === null) {
+    console.log('txOut is null');
+    return false;
+  } else if (typeof txOut.address !== 'string') {
+    console.log('typeof txOut address invalid');
+    return false;
+  } else if (isAddressValid(txOut.address)) {
+    console.log('address is invalid');
+    return false;
+  } else if (typeof txOut.amount !== 'number') {
+    console.log('typeof txOut amount invalid');
+    return false;
+  }
+  return true;
+};
+
+// 주소 검사
+const isAddressValid = (address: string): boolean => {
+  if (address.length <= 130) {
+    return false;
+  } else if (address.match('^[a-fA-F0-9]+$') === null) {
+    return false;
+  } else if (!address.startsWith('04')) {
+    return false;
+  }
+  return true;
+};
+
+const isTxStructureValid = (tx: Transaction): boolean => {
+  if (typeof tx.id !== 'string') {
+    console.log('Tx ID is not valid');
+    return false;
+  } else if (!(tx.txIns instanceof Array)) {
+    console.log('txIns are not an array');
+    return false;
+  } else if (tx.txIns.map(isTxInStructurValid).some(valid => !valid)) {
+    console.log('the structure of on of the txIn is not valid');
+  } else if (!(tx.txOuts instanceof Array)) {
+    console.log('txOuts are not an array');
+    return false;
+  } else if (tx.txOuts.map(isTxOutStructurValid).some(valid => !valid)) {
+    console.log('the structure of on of the txOut is not valid');
+    return false;
+  }
+  return true;
 };
