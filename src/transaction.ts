764f2badbe8651e553535e5d28500a550951c5e3
kirim-Lee <uthi1004@gmail.com>
2019-07-16 22:53:53 +0900

    get transaction id

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,3 +1,5 @@
+import CryptoJS from 'crypto-js';
+
 class TxOut {
   public address: string;
   public amount: number;
@@ -8,23 +10,29 @@ class TxOut {
 }
 
 class TxIn {
+  public uTxOutId: string;
+  public uTxOutIndex: number;
+  public signature: string;
   // uTxOutId
   // uTxOutIndex
   // signature
 }
 
 class Transaction {
+  public ID: string;
+  public txIns: TxIn[];
+  public txOuts: TxOut[];
   // ID
   // txIns[]
   // txOuts[]
 }
 
 class uTxOut {
-  public uTxOutId: number;
+  public uTxOutId: string;
   public uTxOutIndex: number;
   public address: string;
   public amount: number;
-  constructor(uTxOutId: number, uTxOutIndex: number, address: string, amount: number) {
+  constructor(uTxOutId: string, uTxOutIndex: number, address: string, amount: number) {
     this.uTxOutId = uTxOutId;
     this.uTxOutIndex = uTxOutIndex;
     this.address = address;
@@ -33,3 +41,15 @@ class uTxOut {
 }
 
 const uTxOuts: uTxOut[] = [];
+
+const getTxId = (tx: Transaction) => {
+  const txInContent: string = tx.txIns
+    .map((txIn: TxIn) => txIn.uTxOutId + txIn.uTxOutIndex)
+    .reduce((a, b) => a + b, '');
+
+  const txOutContent: string = tx.txOuts
+    .map((txOut: TxOut) => txOut.address + txOut.amount)
+    .reduce((a, b) => a + b, '');
+
+  return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
